162d3f08e62e69ea60c4ba926577d858c8096e67
jamestvu <jamesvu@hawaii.edu>
2013-10-06 21:29:26 -1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-Surferpedia
-===========
+![screenshot](https://raw.github.com/jamestvu/Surferpedia/master/doc/ss.png)
+
+Overview
+
+A play application that provides an encylopedia of surfers.
+
+Installation
+
+Download the zip file, unzip, cd into the directory, invoke the Play console, and execute
+the run command to see the application at http://localhost:9000
+
+Usage
+
+Use the navbar to find interior pages with bio information on surfers.
+
+Credits
+
+Thanks to ICS314, Fall 2013 for support.
---
+++ app/controllers/Application.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.Index;
+import views.html.Eddie;
+import views.html.Daize;
+import views.html.John;
+
+/**
+ * Implements the controllers for this application.
+ */
+public class Application extends Controller {
+
+  /**
+   * Returns the home page. 
+   * @return The resulting home page. 
+   */
+  public static Result index() {
+    return ok(Index.render("Welcome to the home page."));
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result eddie() {
+    return ok(Eddie.render("Eddie Aikau"));
+    
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result john() {
+    return ok(John.render("Eddie Aikau"));
+    
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result daize() {
+    return ok(Daize.render("Eddie Aikau"));
+    
+  }
+}
